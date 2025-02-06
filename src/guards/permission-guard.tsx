0d5394aa2b3d0d7c9ec08d082a2cfeae45f6acdd
noumanishtiaq53 <noumanishtiaq53@gmail.com>
2025-02-06 07:42:22 +0500

    fix(): fix the skeletons (#222)

--- src/guards/permission-guard.tsx
+++ src/guards/permission-guard.tsx
@@ -1,1 +1,18 @@
+export const bypassPermissionsDictionary: any = {};
+
+function checkPermissions(userPermissions: any, modulePermissions: any) {
+  if (!modulePermissions?.length) return false;
+  if (
+    modulePermissions.some((perm: string) => bypassPermissionsDictionary[perm])
+  ) {
+    return true;
+  }
+
+  if (!userPermissions?.length) return false;
+
+  const modulePermissionsSet = new Set(modulePermissions);
+
+  return userPermissions.some((perm: string) => modulePermissionsSet.has(perm));
+}
+
 export const PermissionGuard = () => {};
